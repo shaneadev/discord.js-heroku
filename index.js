ec60805bcdf7008231914717843f2c68ded84278
shaneadev <43152147+shaneadev@users.noreply.github.com>
2018-09-21 23:47:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.mutes = require("./mutes.json");
-
 client.on('ready', () => {
     client.user.setActivity('shane.mp4', {type: 'WATCHING'});
 });
@@ -23,6 +21,17 @@ client.on('message', msg => {
     if (command === 'sal') return msg.channel.send('Salutare!');
     else if (command === 'pa') return msg.channel.send('Ce pa? Poate vrei sa te tau!');
     else if (command === 're') return msg.channel.send('Re băjatu!');
+    
+    msg = message.content.toLowerCase();
+    if(message.author.bot) return;
+    mention = message.mentions.users.first();
+    if(msg.startsWith ("!botmessage")) {
+        if(mention == null) { return; }
+        message.delete();
+        mentionMessage = message.content.slice (8);
+        mention.sendMessage (mentionMessage);
+        message.channel.send ("Mesaj primis cu succes!");
+    }
 });
 
 client.login(process.env.TOKEN);
