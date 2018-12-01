2198e6c95c6409507108d195b017a068bf4c7ad9
shaneadev <43152147+shaneadev@users.noreply.github.com>
2018-12-01 22:12:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ client.on('message', msg => {
     if (command === 'sal') {
         if(cooldown.has(msg.author.id)) {
             msg.delete();
-            return msg.reply("Trebuie sa astepti 3 minute pentru a folosi din nou comenzile!");
+            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
         }
         else msg.channel.send('Salutare!');
         
@@ -34,38 +34,122 @@ client.on('message', msg => {
             cooldown.delete(msg.author.id)  
         }, cdseconds * 1000)
     }
-    else if (command === 'pa') return msg.channel.send('Ce pa? Poate vrei sa te tau!');
-    else if (command === 're') return msg.channel.send('Re băjatu!');
-    else if (command === 'thespriteboss') return msg.channel.send('TheSprite e sheful tuturor!');
+    else if (command === 'pa') {
+        if(cooldown.has(msg.author.id)) {
+            msg.delete();
+            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
+        }
+        else msg.channel.send('Ce pa? Poate vrei sa te tau!');
+        
+        cooldown.add(msg.author.id);   
+
+        setTimeout(() => {
+            cooldown.delete(msg.author.id)  
+        }, cdseconds * 1000)
+    }
+    else if (command === 're') {
+        if(cooldown.has(msg.author.id)) {
+            msg.delete();
+            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
+        }
+        else msg.channel.send('Re băjatu!');
+        
+        cooldown.add(msg.author.id);   
+
+        setTimeout(() => {
+            cooldown.delete(msg.author.id)  
+        }, cdseconds * 1000)
+    }
+    else if (command === 'thespriteboss') {
+        if(cooldown.has(msg.author.id)) {
+            msg.delete();
+            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
+        }
+        else msg.channel.send('TheSprite e sheful tuturor!');
+        
+        cooldown.add(msg.author.id);   
+
+        setTimeout(() => {
+            cooldown.delete(msg.author.id)  
+        }, cdseconds * 1000)
+    }
     else if (command === 'iarina') {
-        msg.channel.send('shane: Langa tine uit de necazurile toate.. Tu imi dai putere sa merg mai departe.. tuu.. cu iubirea ta! @Iarina');
-        msg.channel.send('https://www.youtube.com/watch?v=aGG52QNC678');
+        if(cooldown.has(msg.author.id)) {
+            msg.delete();
+            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
+        }
+        else {
+            msg.channel.send('shane: Langa tine uit de necazurile toate.. Tu imi dai putere sa merg mai departe.. tuu.. cu iubirea ta! @Iarina');
+            msg.channel.send('https://www.youtube.com/watch?v=aGG52QNC678');
+        }
+        
+        cooldown.add(msg.author.id);   
+
+        setTimeout(() => {
+            cooldown.delete(msg.author.id)  
+        }, cdseconds * 1000)
     }
     else if(command === 'profile') {
-        let user = msg.mentions.users.first() || msg.author;
-        let embed = new Discord.RichEmbed()
-        .setAuthor(`Poza de profil a lui ${user.username} este:`)
-        .setImage(user.displayAvatarURL)
-        .setColor('RANDOM')
-        msg.channel.send(embed)
+        if(cooldown.has(msg.author.id)) {
+            msg.delete();
+            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
+        }
+        else {
+            let user = msg.mentions.users.first() || msg.author;
+            let embed = new Discord.RichEmbed()
+            .setAuthor(`Poza de profil a lui ${user.username} este:`)
+            .setImage(user.displayAvatarURL)
+            .setColor('RANDOM')
+            msg.channel.send(embed)
+        }
+        
+        cooldown.add(msg.author.id);   
+
+        setTimeout(() => {
+            cooldown.delete(msg.author.id)  
+        }, cdseconds * 1000)
     }
     else if(command === 'commands') {
-        let embed = new Discord.RichEmbed()
-        .setAuthor('Comenzile botului sunt:')
-        .setDescription('!profile <@ user> - iti arata poza de profil a unui membru/sau poza ta\n!sal - Salutare de la bot\n!re - Re de la bot\n!pa - Amenintari de la bot')
-        .setColor('RANDOM')
-        msg.channel.send(embed)
+        if(cooldown.has(msg.author.id)) {
+            msg.delete();
+            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
+        }
+        else {
+            let embed = new Discord.RichEmbed()
+            .setAuthor('Comenzile botului sunt:')
+            .setDescription('!profile <@ user> - iti arata poza de profil a unui membru/sau poza ta\n!sal - Salutare de la bot\n!re - Re de la bot\n!pa - Amenintari de la bot')
+            .setColor('RANDOM')
+            msg.channel.send(embed)
+        }
+        
+        cooldown.add(msg.author.id);   
+
+        setTimeout(() => {
+            cooldown.delete(msg.author.id)  
+        }, cdseconds * 1000)
     }
     else if(command === 'reclama') {
-        if(!msg.member.permissions.has('ADMINISTRATOR')) return;
-        let msgchannel = msg.guild.channels.find(`name`, "general");
-        if(!msgchannel) return;
-        let embed = new Discord.RichEmbed()
-        .setAuthor('Announcements:')
-        .setDescription('Sustine serverul de discord cu o distribuire a urmatorului link:\nInvite: https://discord.gg/Uj8qkYD')
-        .setColor('RANDOM')
-        msg.delete()
-        return msgchannel.send(embed)
+        if(cooldown.has(msg.author.id)) {
+            msg.delete();
+            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
+        }
+        else {
+            if(!msg.member.permissions.has('ADMINISTRATOR')) return;
+            let msgchannel = msg.guild.channels.find(`name`, "general");
+            if(!msgchannel) return;
+            let embed = new Discord.RichEmbed()
+            .setAuthor('Announcements:')
+            .setDescription('Sustine serverul de discord cu o distribuire a urmatorului link:\nInvite: https://discord.gg/Uj8qkYD')
+            .setColor('RANDOM')
+            msg.delete()
+            return msgchannel.send(embed)
+        }
+        
+        cooldown.add(msg.author.id);   
+
+        setTimeout(() => {
+            cooldown.delete(msg.author.id)  
+        }, cdseconds * 1000)
     }
 });
 
