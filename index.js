f1636b90581c4b60ea9b9c25feb2139ffeb4dacb
shaneadev <43152147+shaneadev@users.noreply.github.com>
2019-05-09 15:51:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -61,7 +61,47 @@ client.on('message', msg => {
     const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
     const args = msg.content.split(' ').slice(1).join(' ');
     
-    if (command === 'sal') {
+    if (command === 'serverinfo') {
+	    function checkDays(date) {
+		let now = new Date();
+		let diff = now.getTime() - date.getTime();
+		let days = Math.floor(diff / 86400000);
+		return days + (days == 1 ? " day" : " days") + " ago";
+	    };
+	    let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
+	    let region = {
+		"brazil": ":flag_br: Brazil",
+		"eu-central": ":flag_eu: Central Europe",
+		"singapore": ":flag_sg: Singapore",
+		"us-central": ":flag_us: U.S. Central",
+		"sydney": ":flag_au: Sydney",
+		"us-east": ":flag_us: U.S. East",
+		"us-south": ":flag_us: U.S. South",
+		"us-west": ":flag_us: U.S. West",
+		"eu-west": ":flag_eu: Western Europe",
+		"vip-us-east": ":flag_us: VIP U.S. East",
+		"london": ":flag_gb: London",
+		"amsterdam": ":flag_nl: Amsterdam",
+		"hongkong": ":flag_hk: Hong Kong",
+		"russia": ":flag_ru: Russia",
+		"southafrica": ":flag_za:  South Africa"
+	    };
+	    const embed = new Discord.RichEmbed()
+		.setAuthor(msg.guild.name, msg.guild.iconURL)
+		.addField("Name", msg.guild.name, true)
+		.addField("ID", msg.guild.id, true)
+		.addField("Owner", `${msg.guild.owner.user.username}#${msg.guild.owner.user.discriminator}`, true)
+		.addField("Region", region[msg.guild.region], true)
+		.addField("Total | Humans | Bots", `${msg.guild.members.size} | ${msg.guild.members.filter(member => !member.user.bot).size} | ${msg.guild.members.filter(member => member.user.bot).size}`, true)
+		.addField("Verification Level", verifLevels[msg.guild.verificationLevel], true)
+		.addField("Channels", msg.guild.channels.size, true)
+		.addField("Roles", msg.guild.roles.size, true)
+		.addField("Creation Date", `${msg.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(msg.channel.guild.createdAt)})`, true)
+		.setThumbnail(msg.guild.iconURL)
+	    msg.channel.send({embed});   
+    }
+	
+    else if (command === 'sal') {
         if(cooldown.has(msg.author.id)) {
             msg.delete();
             return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
