6e1a117b83b3e2ffa9afa028cc1fbfa12a36790a
shaneadev <43152147+shaneadev@users.noreply.github.com>
2019-05-09 16:03:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -69,35 +69,18 @@ client.on('message', msg => {
 		return days + (days == 1 ? " day" : " days") + " ago";
 	    };
 	    let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
-	    let region = {
-		"brazil": ":flag_br: Brazil",
-		"eu-central": ":flag_eu: Central Europe",
-		"singapore": ":flag_sg: Singapore",
-		"us-central": ":flag_us: U.S. Central",
-		"sydney": ":flag_au: Sydney",
-		"us-east": ":flag_us: U.S. East",
-		"us-south": ":flag_us: U.S. South",
-		"us-west": ":flag_us: U.S. West",
-		"eu-west": ":flag_eu: Western Europe",
-		"vip-us-east": ":flag_us: VIP U.S. East",
-		"london": ":flag_gb: London",
-		"amsterdam": ":flag_nl: Amsterdam",
-		"hongkong": ":flag_hk: Hong Kong",
-		"russia": ":flag_ru: Russia",
-		"southafrica": ":flag_za:  South Africa"
-	    };
 	    const embed = new Discord.RichEmbed()
 		.setAuthor(msg.guild.name, msg.guild.iconURL)
 		.addField("Name", msg.guild.name, true)
 		.addField("ID", msg.guild.id, true)
 		.addField("Owner", `${msg.guild.owner.user.username}#${msg.guild.owner.user.discriminator}`, true)
-		.addField("Region", region[msg.guild.region], true)
-		.addField("Total | Humans | Bots", `${msg.guild.members.size} | ${msg.guild.members.filter(member => !member.user.bot).size} | ${msg.guild.members.filter(member => member.user.bot).size}`, true)
-		.addField("Verification Level", verifLevels[msg.guild.verificationLevel], true)
+		.addField("Members", `${msg.guild.members.size}`, true)
+		.addField("Verification level", verifLevels[msg.guild.verificationLevel], true)
 		.addField("Channels", msg.guild.channels.size, true)
 		.addField("Roles", msg.guild.roles.size, true)
-		.addField("Creation Date", `${msg.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(msg.channel.guild.createdAt)})`, true)
+		.addField("Creation date", `${msg.channel.guild.createdAt.toUTCString().substr(0, 16)} (${checkDays(msg.channel.guild.createdAt)})`, true)
 		.setThumbnail(msg.guild.iconURL)
+	    	.setColor('#3388d2')
 	    msg.channel.send({embed});   
     }
 	
