c35f2671ff2362dabb9db8759232ed2126adb581
shaneadev <43152147+shaneadev@users.noreply.github.com>
2019-05-30 10:41:41 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,115 +2,68 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-let cooldown = new Set();
-let cdseconds = 180;
+const config = require("./config.json");
+const no_acces = "nu ai acces la aceasta comanda!";
+const server_status = {
+    total_users_ID: '487210345702621184',
+    member_count_ID: '487210346424172544',
+    bot_count_ID: '570627933337681939'
+}
 
-client.on('ready', () => {
-    client.user.setStatus('available')
-    client.user.setPresence({
-        game: {
-            name: 'San Andreas Universe',
-            type: "STREAMING",
-            url: "https://www.twitch.tv/mue"
-        }
-    });
-});
+client.on('guildMemberAdd', member => {
+    var joinRole = member.guild.roles.find(role => role.name === "Member");
+    member.addRole(joinRole);
 
-client.on("guildMemberAdd", member => {
-    var joinrole = member.guild.roles.find('name', 'Member')
-    member.addRole(joinrole)
-	
-    let joinchannel = member.guild.channels.find('name', 'secrets');
-    joinchannel.send(`**[+]** Alo verutzii! ${member} s-a alaturat acestui grup!`)
-});
+    let joinChannel = member.guild.channels.find(channel => channel.name === "general");
+    joinChannel.send(`**[+]** Alo verutziii! ${member} s-a alaturat acestui grup!`);
+
+    const embed = new Discord.RichEmbed()
+        .setAuthor(`Bine ai venit pe serverul San Andreas Universe, ${member.displayName}!`)
+        .setDescription('**Iti uram sedere placuta alaturi de noi.\nDaca ai intrebari, ni le poti adresa pe chatul <#general>.\n\nO zi/seara/dimineata placuta :wink:!**')
+        .setThumbnail('https://cdn.discordapp.com/icons/285793218023653376/7301f7da88defd2d47f18879fd3b8577.jpg')
+        .setColor('#3388d2')
+        .setTimestamp()
+        .setFooter('joined', 'https://cdn.discordapp.com/icons/285793218023653376/7301f7da88defd2d47f18879fd3b8577.jpg');
+    member.user.send(embed);
+
+    //server status
+    client.channels.get(server_status.total_users_ID).setName(`total members: ${member.guild.memberCount}`);
+    client.channels.get(server_status.member_count_ID).setName(`human count: ${member.guild.members.filter(m => !m.user.bot).size}`);
+    client.channels.get(server_status.bot_count_ID).setName(`bot count: ${member.guild.members.filter(m => m.user.bot).size}`);
+})
+
+client.on('guildMemberRemove', member => {
+    //server status
+    client.channels.get(server_status.total_users_ID).setName(`total members: ${member.guild.memberCount}`);
+    client.channels.get(server_status.member_count_ID).setName(`human count: ${member.guild.members.filter(m => !m.user.bot).size}`);
+    client.channels.get(server_status.bot_count_ID).setName(`bot count: ${member.guild.members.filter(m => m.user.bot).size}`);
+})
 
 client.on('message', msg => {
-    if(msg.content == "clear chat") {
-        if(msg.member.hasPermission("MANAGE_MESSAGES")) {
-            msg.channel.fetchMessages()
-               .then(function(list){
-                msg.channel.bulkDelete(list);
-	    }, function(err){msg.channel.send("Error: I can't clear this channel.")})                        
-        }
-    }
+    if(msg.content.indexOf(config.prefix) !== 0) return;
+    const args = msg.content.slice(config.prefix.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase()
 
-    const thisWord2 = "botule pls ameninta pe";
-    if(msg.content.includes(thisWord2)) {
-    	if(cooldown.has(msg.author.id)) {
-            msg.delete();
-            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
-        }
-
-        number = 10;
-	imageNumber = Math.floor (Math.random() * (number - 1 + 1)) + 1;
-	msg.channel.send( {files: ["./images/" + imageNumber + ".jpg"]});
-	cooldown.add(msg.author.id);   
-
-        setTimeout(() => {
-            cooldown.delete(msg.author.id)
-        }, cdseconds * 1000)
-    }
-	
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
-    const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
-    const args = msg.content.split(' ').slice(1).join(' ');
-    
-    if (command === 'sal') {
-        if(cooldown.has(msg.author.id)) {
-            msg.delete();
-            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
-        }
-        msg.channel.send('Salutare!');
-        
-        cooldown.add(msg.author.id);   
-
-        setTimeout(() => {
-            cooldown.delete(msg.author.id)  
-        }, cdseconds * 1000)
-    }
-    else if (command === 're') {
-        if(cooldown.has(msg.author.id)) {
-            msg.delete();
-            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
-        }
-        msg.channel.send('Re băjatu!');
-        
-        cooldown.add(msg.author.id);   
-
-        setTimeout(() => {
-            cooldown.delete(msg.author.id)  
-        }, cdseconds * 1000)
-    }
-    else if(command === 'reclama') {
-        if(cooldown.has(msg.author.id)) {
-            msg.delete();
-            return msg.reply("trebuie sa astepti 3 minute pentru a folosi din nou aceasta comanda!");
-        }
-        if(!msg.member.permissions.has('ADMINISTRATOR')) return;
-        let msgchannel = msg.guild.channels.find('name', 'general');
-        if(!msgchannel) return;
+    if(command === "ads") {
+        if(!msg.member.permissions.has('ADMINISTRATOR')) return msg.reply(no_acces);
+        let msgChannel = msg.guild.channels.find(channel => channel.name === "general");
+        if(!msgChannel) return msg.reply("nu pot gasit canalul 'general'.");
         let embed = new Discord.RichEmbed()
         .setAuthor('Announcements:')
-        .setDescription('Sustine serverul de discord cu o distribuire a urmatorului link:\nInvite: https://discord.gg/Uj8qkYD')
+        .setDescription('Sustine serverul de discord cu o distribuire a urmatorului link:\nInvite: https://discord.gg/zYPquPB')
         .setColor('#3388d2')
-        msg.delete()
-        msgchannel.send(embed)
-        
-        cooldown.add(msg.author.id);   
-
-        setTimeout(() => {
-            cooldown.delete(msg.author.id)  
-        }, cdseconds * 1000)
+        msg.delete();
+        msgChannel.send(embed);
     }
-});
-
-client.on("channelCreate", async channel => {
-    let sChannel =channel.guild.channels.find(`name`, "logs"); 
-    sChannel.send(`Canalul ***${channel}*** a fost creat cu succes!`);
-});
-client.on("channelDelete", async channel => {
-    let sChannel =channel.guild.channels.find(`name`, "logs"); 
-    sChannel.send(`Canalul ***${channel}*** a fost sters, toate mesajele au fost stocate!`);
-});
+    else if(command === "clear") {
+        if(msg.member.hasPermission("MANAGE_MESSAGES")) {
+            msg.channel.fetchMessages()
+               .then(function(list) {
+                    msg.channel.bulkDelete(list);
+                    msg.reply("chat cleared!");
+            }, function(err){msg.channel.send("Eroare: Nu pot sterge mesajele acestui canal.")})                        
+        }
+    }
+})
 
 client.login(process.env.TOKEN);
