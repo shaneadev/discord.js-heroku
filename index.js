2d69d7a6d0efc3f9789a6722ebdb7b4af3ba9a8d
shaneadev <43152147+shaneadev@users.noreply.github.com>
2019-06-04 22:20:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,42 +1,54 @@
-// Discord.js bot
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 const config = require("./config.json");
-const no_acces = "nu ai acces la aceasta comanda!";
+
 const server_status = {
-    total_users_ID: '487210345702621184',
-    member_count_ID: '487210346424172544',
-    bot_count_ID: '570627933337681939'
+    total_users_ID: '585543386845544491',
+    member_count_ID: '585543852820004864',
+    bot_count_ID: '585543906746171403'
 }
 
-client.on('guildMemberAdd', member => {
-    var joinRole = member.guild.roles.find(role => role.name === "Member");
-    member.addRole(joinRole);
+let cooldown = new Set();
+let cdseconds = 180;
 
-    let joinChannel = member.guild.channels.find(channel => channel.name === "general");
-    joinChannel.send(`**[+]** Alo verutziii! ${member} s-a alaturat acestui grup!`);
+client.on('ready', () => {
+    console.log('I am online!');
+})
+
+client.on('guildMemberAdd', member => {
+    //var join_role = member.guild.find(role => role.name == 'Member');
+    //member.addRole(join_role);
+    
+    let join_channel = client.channels.get('585482694918668290')
+    join_channel.send(`Hey! ${member} s-a alaturat grupului. Salut ${member}, bine ai venit!`);
 
     const embed = new Discord.RichEmbed()
-        .setAuthor(`Bine ai venit pe serverul San Andreas Universe, ${member.displayName}!`)
+        .setAuthor(`Bine ai venit pe serverul SA-MP Forever, ${member.displayName}!`)
         .setDescription('**Iti uram sedere placuta alaturi de noi.\nDaca ai intrebari, ni le poti adresa pe chatul <#general>.\n\nO zi/seara/dimineata placuta :wink:!**')
-        .setThumbnail('https://cdn.discordapp.com/icons/285793218023653376/7301f7da88defd2d47f18879fd3b8577.jpg')
-        .setColor('#3388d2')
+        .setThumbnail('https://i.imgur.com/N20QRLC.jpg')
+        .setColor('#3EB32D')
         .setTimestamp()
-        .setFooter('joined', 'https://cdn.discordapp.com/icons/285793218023653376/7301f7da88defd2d47f18879fd3b8577.jpg');
+        .setFooter('joined', 'https://i.imgur.com/N20QRLC.jpg');
     member.user.send(embed);
 
     //server status
-    client.channels.get(server_status.total_users_ID).setName(`total members: ${member.guild.memberCount}`);
-    client.channels.get(server_status.member_count_ID).setName(`human count: ${member.guild.members.filter(m => !m.user.bot).size}`);
-    client.channels.get(server_status.bot_count_ID).setName(`bot count: ${member.guild.members.filter(m => m.user.bot).size}`);
+    let users_channel = client.channels.get(server_status.total_users_ID)
+    users_channel.setName(`total members: ${member.guild.memberCount}`);
+    let human_channel = client.channels.get(server_status.member_count_ID)
+    human_channel.setName(`human count: ${member.guild.members.filter(m => !m.user.bot).size}`);
+    let bot_channel = client.channels.get(server_status.bot_count_ID)
+    bot_channel.setName(`bot count: ${member.guild.members.filter(m => m.user.bot).size}`);
 })
 
 client.on('guildMemberRemove', member => {
     //server status
-    client.channels.get(server_status.total_users_ID).setName(`total members: ${member.guild.memberCount}`);
-    client.channels.get(server_status.member_count_ID).setName(`human count: ${member.guild.members.filter(m => !m.user.bot).size}`);
-    client.channels.get(server_status.bot_count_ID).setName(`bot count: ${member.guild.members.filter(m => m.user.bot).size}`);
+    let users_channel = client.channels.get(server_status.total_users_ID)
+    users_channel.setName(`total members: ${member.guild.memberCount}`);
+    let human_channel = client.channels.get(server_status.member_count_ID)
+    human_channel.setName(`human count: ${member.guild.members.filter(m => !m.user.bot).size}`);
+    let bot_channel = client.channels.get(server_status.bot_count_ID)
+    bot_channel.setName(`bot count: ${member.guild.members.filter(m => m.user.bot).size}`);
 })
 
 client.on('message', msg => {
@@ -45,24 +57,32 @@ client.on('message', msg => {
     const command = args.shift().toLowerCase()
 
     if(command === "ads") {
-        if(!msg.member.permissions.has('ADMINISTRATOR')) return msg.reply(no_acces);
-        let msgChannel = msg.guild.channels.find(channel => channel.name === "general");
-        if(!msgChannel) return msg.reply("nu pot gasit canalul 'general'.");
-        let embed = new Discord.RichEmbed()
-        .setAuthor('Announcements:')
-        .setDescription('Sustine serverul de discord cu o distribuire a urmatorului link:\nInvite: https://discord.gg/zYPquPB')
-        .setColor('#3388d2')
-        msg.delete();
-        msgChannel.send(embed);
+        if(!msg.member.permissions.has('ADMINISTRATOR')) return msg.reply("nu ai acces la aceasta comanda!");
+        let message_channel = msg.mentions.channels.first();
+        if(message_channel) {
+            let embed = new Discord.RichEmbed()
+            .setAuthor('Announcements:')
+            .setDescription('Sustine serverul de discord cu o distribuire a urmatorului link:\nInvite: https://discord.gg/UyAUtrE')
+            .setColor('#3EB32D')
+            msg.delete();
+            message_channel.send(embed);
+        } else {
+            let embed = new Discord.RichEmbed()
+            .setAuthor('Announcements:')
+            .setDescription('Sustine serverul de discord cu o distribuire a urmatorului link:\nInvite: https://discord.gg/UyAUtrE')
+            .setColor('#3EB32D')
+            msg.delete();
+            msg.channel.send(embed);
+        }
     }
-    else if(command === "clear") {
+    else if(command === "cc") {
         if(msg.member.hasPermission("MANAGE_MESSAGES")) {
             msg.channel.fetchMessages()
                .then(function(list) {
                     msg.channel.bulkDelete(list);
                     msg.reply("chat cleared!");
             }, function(err){msg.channel.send("Eroare: Nu pot sterge mesajele acestui canal.")})                        
-        }
+        } else { msg.reply("nu ai acces la aceasta comanda!"); }
     }
 })
 
